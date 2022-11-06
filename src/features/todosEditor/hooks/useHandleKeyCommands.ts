59f0bd6d0518d03074a5483e53e436dc7332a721
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2022-11-06 08:41:44 +0100

    Automatic todo prefix on enter after todo

--- src/features/todosEditor/hooks/useHandleKeyCommands.ts
+++ src/features/todosEditor/hooks/useHandleKeyCommands.ts
@@ -5,6 +5,8 @@ import {
     RichUtils,
 } from 'draft-js';
 import { OnEditorChangeHandler } from '../TodosEditor';
+import { useTodosContext } from '../../../context/todos/TodosContext';
+import { splitToNewContentBlockWithTodoPrefix } from '../../../utilities/editorStateModifiers';
 
 export type KeyCommandHandler = (
     command: EditorCommand,
@@ -14,10 +16,23 @@ export type KeyCommandHandler = (
 export default function useHandleKeyCommands(
     onChange: OnEditorChangeHandler,
 ): KeyCommandHandler {
+    const { setEditorState } = useTodosContext();
+
     return (
         command: EditorCommand,
         editorState: EditorState,
     ): DraftHandleValue => {
+        if (command === 'split-block') {
+            const newEditorState =
+                splitToNewContentBlockWithTodoPrefix(editorState);
+
+            if (newEditorState) {
+                setEditorState(newEditorState);
+
+                return 'handled';
+            }
+        }
+
         const newState = RichUtils.handleKeyCommand(editorState, command);
 
         if (newState) {
---
+++ src/utilities/editorStateModifiers.ts
@@ -0,0 +1,50 @@
+import { EditorState, Modifier } from 'draft-js';
+import { todoRegex } from '../model/TodoFactory';
+
+export function splitToNewContentBlockWithTodoPrefix(
+    editorState: EditorState,
+): EditorState | null {
+    const currentContentState = editorState.getCurrentContent();
+    const currentContentBlock = currentContentState.getBlockForKey(
+        editorState.getSelection().getStartKey(),
+    );
+
+    const currentContentBlockText = currentContentBlock.getText();
+    const previousIsTodo = todoRegex.test(currentContentBlockText);
+    if (!previousIsTodo) {
+        return null;
+    }
+
+    const indentMatch = currentContentBlockText.match(/^([ ]{0,})/);
+    const indent = indentMatch ? indentMatch[1].length : 0;
+
+    const currentSelection = editorState.getSelection();
+
+    let newContentState = Modifier.splitBlock(
+        currentContentState,
+        currentSelection,
+    );
+
+    const newlyCreatedBlock = newContentState.getBlockAfter(
+        currentContentBlock.getKey(),
+    );
+
+    if (!newlyCreatedBlock) {
+        return null;
+    }
+
+    const textToInsert = `${' '.repeat(indent)}- [ ] `;
+
+    newContentState = Modifier.insertText(
+        newContentState,
+        currentSelection.merge({
+            anchorKey: newlyCreatedBlock.getKey(),
+            anchorOffset: 0,
+            focusKey: newlyCreatedBlock.getKey(),
+            focusOffset: 0,
+        }),
+        textToInsert,
+    );
+
+    return EditorState.push(editorState, newContentState, 'split-block');
+}

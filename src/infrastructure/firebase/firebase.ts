fa2cf0b2acfadcce9ea0d652c4a91bf1bb30192b
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2023-03-26 15:07:36 +0200

    Use .env instead of JSON file for Firebase credentials

--- src/infrastructure/firebase/firebase.ts
+++ src/infrastructure/firebase/firebase.ts
@@ -6,7 +6,6 @@ import {
     signInWithPopup,
 } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import config from './../../config/firebase.json';
 
 let app: FirebaseApp;
 
@@ -15,7 +14,14 @@ export function initializeFirebase(): void {
         throw new Error('Firebase App already initialized');
     }
 
-    const firebaseConfig = config;
+    const firebaseConfig = {
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    };
 
     // Initialize Firebase
     app = initializeApp(firebaseConfig, {

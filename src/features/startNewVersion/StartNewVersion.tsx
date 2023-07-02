0cb5166b45cf04f4d76f01a62df4600e17ea83ac
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2023-07-02 06:52:41 +0200

    #3 Create feature component for start new version

---
+++ src/features/startNewVersion/StartNewVersion.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+const StartNewVersion: FC = () => {
+    return (
+        <button
+            type="button"
+            className="border border-black rounded text-xs px-1"
+        >
+            new
+        </button>
+    );
+};
+
+export default StartNewVersion;
--- src/features/versionsOverview/VersionsOverview.tsx
+++ src/features/versionsOverview/VersionsOverview.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { useTodosContext } from '../../context/todos/TodosContext';
 import Version from './components/Version';
 import VersionList from './components/VersionList';
+import StartNewVersion from '../startNewVersion/StartNewVersion';
 
 const VersionsOverview: FC = () => {
     const { todoLists, currentTodoList } = useTodosContext();
@@ -12,7 +13,10 @@ const VersionsOverview: FC = () => {
 
     return (
         <section className="space-y-2">
-            <h1 className="text-l font-bold">Versions</h1>
+            <div className="flex justify-between">
+                <h1 className="text-l font-bold">Versions</h1>
+                <StartNewVersion />
+            </div>
             <VersionList>
                 {todoLists.map((todoList, index) => (
                     <Version

2c9c9cdd4f67abb4de707ea20b47924396870105
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2023-07-16 09:31:41 +0200

    Small improvement to styling of done todo

--- src/features/todosEditor/components/Checkbox.tsx
+++ src/features/todosEditor/components/Checkbox.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import useKeyOfContentBlockThatHasFocus from '../hooks/useKeyOfContentBlockThatHasFocus';
+import composeClassName from 'classnames';
 
 type Props = {
     children: ReactNode;
@@ -11,7 +12,20 @@ const Checkbox: FC<Props> = ({ children, blockKey }) => {
 
     const className = hasFocus ? 'text-orange-500 font-bold' : 'opacity-30';
 
-    return <span className={className}>{children}</span>;
+    return (
+        <>
+            <span
+                className={className}
+                style={{
+                    // Inline block required to override line-through on parent (@see useResolveContentBlockClassName)
+                    display: 'inline-block',
+                    textDecoration: 'none !important',
+                }}
+            >
+                {children}
+            </span>
+        </>
+    );
 };
 
 export default Checkbox;

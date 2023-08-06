fa055fad22471afd20f91f1441a45033f21ae962
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2023-08-06 07:14:59 +0200

    #18 Check if adding a touch handler can fix problem with mouse interaction with toolbar

--- src/features/toolbar/components/ToolbarItem.tsx
+++ src/features/toolbar/components/ToolbarItem.tsx
@@ -1,4 +1,4 @@
-import { FC, MouseEventHandler } from 'react';
+import { FC, MouseEventHandler, TouchEventHandler } from 'react';
 import { ToolbarAction } from '../hooks/useEnabledActions';
 import ToolbarItemIcon from './ToolbarItemIcon';
 
@@ -17,11 +17,19 @@ const ToolbarItem: FC<Props> = ({ action, handleActionClick }) => {
         handleActionClick(action);
     };
 
+    const onTouchStart: TouchEventHandler = (event): void => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        handleActionClick(action);
+    };
+
     // Using diff instead of button to prevent losing focus in the editor
     // @see https://github.com/facebookarchive/draft-js/issues/696#issuecomment-302903086
     return (
         <div
             onMouseDown={onMouseDown}
+            onTouchStart={onTouchStart}
             aria-roledescription="button"
             role="button"
             className="bg-white border border-gray-400 p-1 hover:border-gray-900 cursor-pointer"
